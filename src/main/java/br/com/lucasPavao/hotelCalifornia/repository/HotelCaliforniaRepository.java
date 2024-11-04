da4531b7028c9eb475c3911ce2f19409b72a5b82
Lucas Pavao <lucaspavao89@gmail.com>
2024-11-04 18:35:12 -0300

    first-commit

---
+++ src/main/java/br/com/lucasPavao/hotelCalifornia/repository/HotelCaliforniaRepository.java
@@ -0,0 +1,13 @@
+package br.com.lucasPavao.hotelCalifornia.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.lucasPavao.hotelCalifornia.model.HotelCaliforniaModel;
+
+@Repository
+public interface HotelCaliforniaRepository extends JpaRepository<HotelCaliforniaModel, UUID>{
+    
+}
