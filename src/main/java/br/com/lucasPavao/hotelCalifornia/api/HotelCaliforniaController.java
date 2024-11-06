0f2d72f81d376ea4b5d94143c77c01ea1e644194
Lucas Henrique Ferreira Pavao <lucas.pavao@mv.com.br>
2024-11-06 09:14:17 -0300

    Criando put e delete

--- src/main/java/br/com/lucasPavao/hotelCalifornia/api/HotelCaliforniaController.java
+++ src/main/java/br/com/lucasPavao/hotelCalifornia/api/HotelCaliforniaController.java
@@ -1,13 +1,17 @@
 package br.com.lucasPavao.hotelCalifornia.api;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,10 +33,37 @@ public class HotelCaliforniaController {
     public List<HotelCaliforniaModel> hotelCaliforniaGetAll() {
         return repository.findAll();
     }
-
+  
     @PostMapping
     public ResponseEntity<HotelCaliforniaModel> createHotel(@RequestBody HotelCaliforniaModel hotel) {
         HotelCaliforniaModel savedHotel = repository.save(hotel);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedHotel);
     }
+
+    @PutMapping
+    public ResponseEntity<HotelCaliforniaModel> updateHotel(@RequestBody HotelCaliforniaModel hotel) {
+       
+        if (repository.existsById(hotel.getId())) {
+            HotelCaliforniaModel updatedHotel = repository.findById(hotel.getId()).get();
+            updatedHotel.setName(hotel.getName());
+            updatedHotel.setLocal(hotel.getLocal());
+            updatedHotel.setCapacidade(hotel.getCapacidade());
+            updatedHotel.setCnpj(hotel.getCnpj());
+            return ResponseEntity.ok(updatedHotel);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteHotel(@PathVariable UUID id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
 }
